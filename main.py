8646a10b409c886e48addce225ece8a8119d59e7
iwaveM <mqodir201123@gmail.com>
2024-08-23 20:28:58 +0500

    github

---
+++ main.py
@@ -0,0 +1,16 @@
+# 1
+# A = input("Make it so I can see the weather")
+
+#2 
+# a = ()
+# b = []
+# A = input("Find the difference?")
+
+# 3
+# A = input("Give me two value for bool")
+
+# 4
+# A = input("What does sort do? ")
+
+# 5
+# A = "Make calculator"   
